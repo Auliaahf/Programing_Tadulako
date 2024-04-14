17e88b7f30ee8f293ce2f9c37cf1759aa31c7380
Aulia Fitri <eltrhanifah@gmail.com>
2024-04-14 23:05:04 +0800

    add week 9 & 10

---
+++ week9/index.js
@@ -0,0 +1,44 @@
+// function iyak(){
+//     console.log("aloo yak!")
+// }
+
+// iyak();
+
+// function luasPersegiPanjang(Panjang, Lebar){
+//     let luas = Panjang * Lebar
+//     console.log(luas)
+// }
+
+// let luasPersegi = function(sisi){
+//     let luas = sisi * sisi;
+//     console.log(luas);
+// };
+
+// const luasSegitiga = (alas, tinggi) => {
+//     let luas =  1/2 * alas * tinggi;
+//     console.log(luas);
+// };
+
+// luasPersegiPanjang(6, 2)
+// luasPersegi(7)
+// luasSegitiga(2, 3)
+
+// const luasLingkaran = (r) =>{
+//     return 3.14 * r *r;
+// };
+
+// // const luasLingkaran = (r) = 3.14 * r * r;
+// // cara tanpa return ^^^
+
+// console.log(luasLingkaran(4));
+
+/
+
+function tampilkan_seluruh_nama(arr_nama){
+    pan = arr_nama.length
+    For(let i = 0; i < pan; i++){
+        console.log(arr_nama[i])
+    }
+}
+
+tampilkan_seluruh_nama([iyak, sifa, nay, sahira, fatira])
